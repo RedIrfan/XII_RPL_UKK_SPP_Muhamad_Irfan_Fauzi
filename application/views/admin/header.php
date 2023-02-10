45282ab84dee82b9e028d245207672a6349211c5
RedIrfan <iniirfanfauzi@gmail.com>
2023-02-10 09:32:22 +0700

    added sidebar that changes to offcanvas

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -9,17 +9,21 @@
         background-color: white;
         transition: 0.1s !important;
     }
+    .w-0{
+        width: 0 !important;
+    }
 </style>
 <link href="<?= base_url('css/styles.css') ?>" rel="stylesheet" />
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<div class="navbar navbar-expand-lg bg-navbar navbar-dark text-white fixed-top">
-    <div class='container-fluid justify-content-start py-2 d-flex justify-content-between justify-content-lg-start'>
-        <a class="navbar-brand col-2 me-0 ps-3" href="<?= site_url('admin') ?>">Irfan SPP</a>    
+<div class="navbar bg-navbar navbar-dark text-white fixed-top">
+    <div class='container-fluid justify-content-start py-2 d-flex justify-content-between justify-content-md-start'>
+        <a class="navbar-brand me-0 ps-3" style="width:205px;" href="<?= site_url('admin') ?>">Irfan SPP</a>    
         <div class='d-flex'>
-            <a class="nav-link pe-3" id="offcanvas" href="#collapse-content" data-bs-toggle="collapse" data-bs-target="#collapse-content" aria-controls="collapse-content" aria-expanded="false"><i class="fas fa-bars"></i></a>
+            <p id="tes"></p>
+            <a class="nav-link pe-3 collapse-link" id="collapse-link" href="#collapse-content" data-bs-toggle="collapse" data-bs-target="#collapse-content" aria-controls="collapse-content" aria-expanded="false"><i class="fas fa-bars"></i></a>
             <div class='dropdown'>
                 <a class="nav-link dropdown-toggle" id="dropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user"></i></a>
                 <ul class="dropdown-menu dropdown-menu-start" aria-labelledby="dropdown">
@@ -31,8 +35,8 @@
 </div>
 
 <div>
-    <div class="collapse collapse-horizontal show sidenav col-2  border-end border-2" id="collapse-content" style="position:fixed; margin-top:56px;">
-        <div class="container mb-auto" style="height:81vh;">
+    <div class="collapse collapse-horizontal show sidenav border-end border-2" id="collapse-content" style="position:fixed; padding-top:56px;">
+        <div class="container mb-auto" style="height:81vh; width:205px;">
             <ul class="nav flex-column px-2 pt-3">
                 <li class="nav-item">
                     <h6 class="pt-4 fw-bolder">CORE</h6>
@@ -57,8 +61,8 @@
     </div>
 </div>
 <div class="mx-0 d-flex h-100" style="margin-top:56px;">
-    <div class="col-2 collapse collapse-horizontal show sidenav" id="collapse-content">
-        <div class="container mb-auto" style="height:81vh"></div>
+    <div class="collapse collapse-horizontal show sidenav collapse-hidden" id="collapse-content" style="width:200px;">
+        <div class="container mb-auto" style="height:81vh; width:200px;"></div>
     </div>
     <div class='w-100 h-100'>
     
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,14 @@
-/*!
-    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2022 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
-// Scripts
-// 
+$(window).resize(function(){
+    var content = $('#collapse-content');
+    var hidden = $('.collapse-hidden');
+    var link = $("#collapse-link");
+    var width = $(window).width();
 
-window.addEventListener('DOMContentLoaded', event => {
-    
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-        });
+    if (width < 768){
+        hidden.removeClass("collapse show").addClass("w-0");
+        content.removeClass("collapse collapse-horizontal show").addClass("offcanvas offcanvas-start");
+    } else {
+        hidden.removeClass('w-0').addClass("collapse show");
+        content.removeClass("offcanvas offcanvas-start").addClass("collapse collapse-horizontal show");
     }
-
 });
